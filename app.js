24111d795eb5f02a3e8a141d12133a52a62b6efc
alejandropl07 <aprietoleon97@gmail.com>
2022-02-24 17:42:10 -0600

    Metodo create hecho

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import store from './api/models/store.js';
 
 const app   =   express();
 const port  =   5500;
 
-const mongoURL  =   "mongodb+srv://alejandropl07:MongoDBConnect@cluster0.rico8.mongodb.net/clientes?retryWrites=true&w=majority"
+const mongoURL  =   "mongodb+srv://alejandropl07:mongoDBConnect@cluster0.rico8.mongodb.net/clientes?retryWrites=true&w=majority"
 
 mongoose.connect(mongoURL, {useNewUrlParser:true, useUnifiedTopology:true})
 
 app.use(express.json({limit:"50mb"}))
 
 app.post("/api/clients",    (req,res)   =>  {
-    console.log("dummy EndPoint");
-    res.send("You have posted something")
+    let clientData  =   req.body
+    let mongoRecords    =   []
+    clientData.forEach(client => {
+        mongoRecords.push({
+            firstName   :   client.firstName,
+            phone   :   client.phone,
+            address   :   client.address
+        })
+    });
+    store.create(mongoRecords,  (err, records)  => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(records)
+        }
+    } )
 })
 
 app.get("/" ,   (req,res)  =>  {
-    res.send("Hello world");
+    res.send("Hello world")
 });
 
 app.listen(port, () =>  {
