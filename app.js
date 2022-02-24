8bcbede5514fee64dac904a88f05e0333d5c9a85
alejandropl07 <aprietoleon97@gmail.com>
2022-02-23 19:19:20 -0600

    Conectado a MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app   =   express();
 const port  =   5500;
+
+const mongoURL  =   "mongodb+srv://alejandropl07:MongoDBConnect@cluster0.rico8.mongodb.net/clientes?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURL, {useNewUrlParser:true, useUnifiedTopology:true})
 
 app.use(express.json({limit:"50mb"}))
 
